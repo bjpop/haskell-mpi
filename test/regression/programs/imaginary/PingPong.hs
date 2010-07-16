14c24196bebab60e59c79bab01f4dcc771587409
Bernie Pope <florbitous@gmail.com>
2010-07-16 23:56:58 +1000

    Improved code to use nicer MPI interface.

--- test/regression/programs/imaginary/PingPong.hs
+++ test/regression/programs/imaginary/PingPong.hs
@@ -1,29 +1,35 @@
 module Main where
 
+import Prelude hiding (init)
 import Control.Monad (when)
-import Bindings.MPI.Internal as MPI
+import Bindings.MPI
+
+tag :: Int
+tag = 42
+
+data Rank = Pinger | Ponger
+   deriving (Enum, Eq)
 
 main :: IO ()
 main = do
-   MPI.init
-   (_, rank) <- MPI.commRank MPI.commWorld
-   when (rank == 0) $ do
-       MPI.send (0::Int) 1 MPI.int 1 42 MPI.commWorld
-       ping
-   when (rank == 1) pong
-   MPI.finalize
+   init
+   (_, rank) <- commRank commWorld
+   send (0::Integer) Ponger tag commWorld
+   when (toEnum rank == Pinger) ping
+   when (toEnum rank == Ponger) pong
+   finalize
    return ()
 
 ping :: IO ()
 ping = do
-   (_, _, i) <- MPI.recv 1 MPI.int 1 42 MPI.commWorld 
-   putStrLn $ "Ping " ++ show (i::Int)
-   MPI.send (i+1) (1::Int) MPI.int 1 42 MPI.commWorld
+   (_, _, i) <- recv Ponger tag commWorld 
+   putStrLn $ "Ping " ++ show (i::Integer)
+   send (i+1) Ponger tag commWorld
    ping
 
 pong :: IO ()
 pong = do 
-   (_, _, i) <- MPI.recv 1 MPI.int 0 42 MPI.commWorld 
-   putStrLn $ "Pong " ++ show (i::Int)
-   MPI.send (i+1) (1::Int) MPI.int 0 42 MPI.commWorld
+   (_, _, i) <- recv Pinger tag commWorld 
+   putStrLn $ "Pong " ++ show (i::Integer)
+   send (i+1) Pinger tag commWorld
    pong
