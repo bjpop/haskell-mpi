e6be98ea80549795f3f4a75e7a2579d0b354ec9a
Bernie Pope <florbitous@gmail.com>
2010-07-17 18:01:42 +1000

    Added iSend

--- src/Bindings/MPI.hs
+++ src/Bindings/MPI.hs
@@ -12,6 +12,7 @@ import Data.Serialize (encode, decode, Serialize)
 import qualified Bindings.MPI.Internal as Internal 
 import Bindings.MPI.Datatype as Datatype
 import Bindings.MPI.Comm as Comm
+import Bindings.MPI.Request as Request
 import Bindings.MPI.Status as Status 
 import Bindings.MPI.MarshalUtils (enumToCInt, enumFromCInt)
 import Bindings.MPI.Utils (checkError)
@@ -22,12 +23,27 @@ init = checkError Internal.init
 finalize :: IO ()
 finalize = checkError Internal.finalize
 
+commSize :: Comm -> IO Int
+commSize comm = do
+   alloca $ \ptr -> do
+      checkError $ Internal.commSize comm ptr
+      size <- peek ptr
+      return $ cIntConv size  
+
 commRank :: Enum rank => Comm -> IO rank
 commRank comm = 
    alloca $ \ptr -> do
       checkError $ Internal.commRank comm ptr
       rank <- peek ptr
       return $ enumFromCInt rank
+
+probe :: (Enum source, Enum tag) => source -> tag -> Comm -> IO Status
+probe source tag comm = do
+   let cSource = enumToCInt source
+       cTag    = enumToCInt tag
+   alloca $ \statusPtr -> do
+      checkError $ Internal.probe cSource cTag comm (castPtr statusPtr)
+      peek statusPtr
 
 send :: (Serialize msg, Enum dest, Enum tag) => msg -> dest -> tag -> Comm -> IO () 
 send = sendBS . encode
@@ -50,23 +66,28 @@ recv source tag comm = do
 -- XXX should probably bracket here to free malloc'd memory on exceptions.
 recvBS :: (Enum source, Enum tag) => source -> tag -> Comm -> IO (Status, ByteString)
 recvBS source tag comm = do
-  probeStatus <- probe source tag comm
-  let count = status_count probeStatus 
-  bufferPtr <- mallocBytes count 
-  let cSource = enumToCInt source
-      cTag    = enumToCInt tag
-      cCount  = cIntConv count
-  alloca $ \ statusPtr -> do
-     checkError $ Internal.recv bufferPtr cCount byte cSource cTag comm (castPtr statusPtr)
-     recvStatus <- peek statusPtr
-     message <- packCStringLen (castPtr bufferPtr, count)  
-     free bufferPtr
-     return (recvStatus, message)
-
-probe :: (Enum source, Enum tag) => source -> tag -> Comm -> IO Status
-probe source tag comm = do
+   probeStatus <- probe source tag comm
+   let count = status_count probeStatus 
+   bufferPtr <- mallocBytes count 
    let cSource = enumToCInt source
        cTag    = enumToCInt tag
-   alloca $ \statusPtr -> do
-      checkError $ Internal.probe cSource cTag comm (castPtr statusPtr)
-      peek statusPtr
+       cCount  = cIntConv count
+   alloca $ \ statusPtr -> do
+      checkError $ Internal.recv bufferPtr cCount byte cSource cTag comm (castPtr statusPtr)
+      recvStatus <- peek statusPtr
+      message <- packCStringLen (castPtr bufferPtr, count)  
+      free bufferPtr
+      return (recvStatus, message)
+
+iSend :: (Serialize msg, Enum dest, Enum tag) => msg -> dest -> tag -> Comm -> IO Request 
+iSend = iSendBS . encode
+
+iSendBS :: (Enum dest, Enum tag) => ByteString -> dest -> tag -> Comm -> IO Request 
+iSendBS bs dest tag comm = do
+   let cDest = enumToCInt dest   
+       cTag  = enumToCInt tag
+       cCount = cIntConv $ BS.length bs
+   alloca $ \requestPtr -> 
+      unsafeUseAsCString bs $ \cString -> do
+          checkError $ Internal.iSend (castPtr cString) cCount byte cDest cTag comm requestPtr
+          peek requestPtr 
