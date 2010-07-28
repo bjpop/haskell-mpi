5eb03a3c42f81d5d2f989ca06080203a8466a6b3
Bernie Pope <florbitous@gmail.com>
2010-07-28 21:09:40 +1000

    Test case for greetings with non blocking recv.

---
+++ test/regression/programs/imaginary/GreetingsAsync.hs
@@ -0,0 +1,34 @@
+-- Based on the example program from page 41/42 of 
+-- Pacheco "Parallel programming with MPI"
+
+module Main where
+
+import Control.Monad (when, forM)
+import Control.Parallel.MPI
+
+msg :: Rank -> String 
+msg r = "Greetings from process " ++ show r ++ "!"
+
+comm :: Comm
+comm = commWorld
+
+root :: Rank
+root = toRank 0
+
+tag :: Tag
+tag = toTag ()
+
+main :: IO ()
+main = mpi $ do
+   rank <- commRank comm 
+   size <- commSize comm 
+   if (rank /= root) 
+      then send (msg rank) root tag comm 
+      else do 
+         futures <- forM [1..size-1] $ \sender -> 
+            recvFuture (toRank sender) tag comm 
+         mapM_ printFuture futures 
+    where
+    printFuture :: Future String -> IO ()
+    printFuture future = putStrLn =<< waitFuture future
+            
