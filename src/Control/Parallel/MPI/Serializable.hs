4d429dd53236f156c8647cf109f5bab7b273c977
Bernie Pope <florbitous@gmail.com>
2010-08-25 21:08:39 +1000

    Renamed MPI to Serializable

--- src/Control/Parallel/MPI/Serializable.hs
+++ src/Control/Parallel/MPI/Serializable.hs
@@ -1,12 +1,12 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 
-module Control.Parallel.MPI 
-   ( module Datatype 
+module Control.Parallel.MPI.Serializable
+   ( module Datatype
    , module Comm
    , module Status
    , module Tag
-   , module Rank 
-   , mpi 
+   , module Rank
+   , mpi
    , init
    , finalize
    , commSize
@@ -39,10 +39,10 @@ import qualified Control.Parallel.MPI.Internal as Internal
 import Control.Parallel.MPI.Datatype as Datatype
 import Control.Parallel.MPI.Comm as Comm
 import Control.Parallel.MPI.Request as Request
-import Control.Parallel.MPI.Status as Status 
+import Control.Parallel.MPI.Status as Status
 import Control.Parallel.MPI.Utils (checkError)
-import Control.Parallel.MPI.Tag as Tag 
-import Control.Parallel.MPI.Rank as Rank 
+import Control.Parallel.MPI.Tag as Tag
+import Control.Parallel.MPI.Rank as Rank
 
 mpi :: IO () -> IO ()
 mpi action = init >> action >> finalize
@@ -58,10 +58,10 @@ commSize comm = do
    alloca $ \ptr -> do
       checkError $ Internal.commSize comm ptr
       size <- peek ptr
-      return $ cIntConv size  
+      return $ cIntConv size
 
 commRank :: Comm -> IO Rank
-commRank comm = 
+commRank comm =
    alloca $ \ptr -> do
       checkError $ Internal.commRank comm ptr
       rank <- peek ptr
@@ -69,21 +69,21 @@ commRank comm =
 
 probe :: Rank -> Tag -> Comm -> IO Status
 probe rank tag comm = do
-   let cSource = fromRank rank 
+   let cSource = fromRank rank
        cTag    = fromTag tag
    alloca $ \statusPtr -> do
       checkError $ Internal.probe cSource cTag comm $ castPtr statusPtr
       peek statusPtr
 
-send :: Serialize msg => msg -> Rank -> Tag -> Comm -> IO () 
+send :: Serialize msg => msg -> Rank -> Tag -> Comm -> IO ()
 send = sendBS . encode
 
-sendBS :: BS.ByteString -> Rank -> Tag -> Comm -> IO () 
+sendBS :: BS.ByteString -> Rank -> Tag -> Comm -> IO ()
 sendBS bs rank tag comm = do
-   let cRank = fromRank rank 
+   let cRank = fromRank rank
        cTag  = fromTag tag
        cCount = cIntConv $ BS.length bs
-   unsafeUseAsCString bs $ \cString -> 
+   unsafeUseAsCString bs $ \cString ->
        checkError $ Internal.send (castPtr cString) cCount byte cRank cTag comm
 
 recv :: Serialize msg => Rank -> Tag -> Comm -> IO (Status, msg)
@@ -92,37 +92,37 @@ recv rank tag comm = do
    case decode bs of
       Left e -> fail e
       Right val -> return (status, val)
-        
+
 recvBS :: Rank -> Tag -> Comm -> IO (Status, BS.ByteString)
 recvBS rank tag comm = do
    probeStatus <- probe rank tag comm
-   let count = status_count probeStatus 
-       cSource = fromRank rank 
+   let count = status_count probeStatus
+       cSource = fromRank rank
        cTag    = fromTag tag
        cCount  = cIntConv count
-   allocaBytes count 
-      (\bufferPtr -> 
+   allocaBytes count
+      (\bufferPtr ->
           alloca $ \statusPtr -> do
              checkError $ Internal.recv bufferPtr cCount byte cSource cTag comm $ castPtr statusPtr
              recvStatus <- peek statusPtr
-             message <- BS.packCStringLen (castPtr bufferPtr, count)  
+             message <- BS.packCStringLen (castPtr bufferPtr, count)
              return (recvStatus, message))
 
-iSend :: Serialize msg => msg -> Rank -> Tag -> Comm -> IO Request 
+iSend :: Serialize msg => msg -> Rank -> Tag -> Comm -> IO Request
 iSend = iSendBS . encode
 
-iSendBS :: BS.ByteString -> Rank -> Tag -> Comm -> IO Request 
+iSendBS :: BS.ByteString -> Rank -> Tag -> Comm -> IO Request
 iSendBS bs rank tag comm = do
-   let cRank = fromRank rank 
+   let cRank = fromRank rank
        cTag  = fromTag tag
        cCount = cIntConv $ BS.length bs
-   alloca $ \requestPtr -> 
+   alloca $ \requestPtr ->
       unsafeUseAsCString bs $ \cString -> do
           checkError $ Internal.iSend (castPtr cString) cCount byte cRank cTag comm requestPtr
-          peek requestPtr 
+          peek requestPtr
 
-data Future a = 
-   Future 
+data Future a =
+   Future
    { futureThread :: ThreadId
    , futureStatus :: MVar Status
    , futureVal :: MVar a
@@ -132,16 +132,16 @@ waitFuture :: Future a -> IO a
 waitFuture = readMVar . futureVal
 
 pollFuture :: Future a -> IO (Maybe a)
-pollFuture = tryTakeMVar . futureVal 
+pollFuture = tryTakeMVar . futureVal
 
 -- May want to stop people from waiting on Futures which are killed...
 cancelFuture :: Future a -> IO ()
 cancelFuture = killThread . futureThread
 
-recvFuture :: Serialize msg => Rank -> Tag -> Comm -> IO (Future msg) 
+recvFuture :: Serialize msg => Rank -> Tag -> Comm -> IO (Future msg)
 recvFuture rank tag comm = do
-   valRef <- newEmptyMVar  
-   statusRef <- newEmptyMVar 
+   valRef <- newEmptyMVar
+   statusRef <- newEmptyMVar
    -- is forkIO acceptable here? Depends on thread local stateness of MPI.
    -- threadId <- forkOS $ do
    threadId <- forkIO $ do
@@ -155,7 +155,7 @@ recvFuture rank tag comm = do
    The C interface assumes the sender and receiver agree on the size in advance, but
    this is not useful for the Haskell interface (where we want to send arbitrary sized
    values) because the sender is the only process which has the actual data available
-   
+
    The work around is for the sender to send two messages. The first says how much data
    is coming. The second message sends the actual data. We rely on the two messages being
    sent and received in this order. Conversely the receiver gets two messages. The first is
@@ -165,20 +165,20 @@ recvFuture rank tag comm = do
    payload. Communication costs can be expensive.
 
    The idea for this scheme was inspired by the Ocaml bindings. Therefore there is
-   some precedent for doing it this way. 
+   some precedent for doing it this way.
 -}
 
 bcast :: Serialize msg => msg -> Rank -> Comm -> IO msg
 bcast msg rootRank comm = do
    myRank <- commRank comm
-   let cRank  = fromRank rootRank 
+   let cRank  = fromRank rootRank
    if myRank == rootRank
-      then do 
+      then do
          let bs = encode msg
-             cCount = cIntConv $ BS.length bs 
+             cCount = cIntConv $ BS.length bs
          -- broadcast the size of the message first
          alloca $ \ptr -> do
-            poke ptr cCount 
+            poke ptr cCount
             let numberOfInts = 1::CInt
             checkError $ Internal.bcast (castPtr ptr) numberOfInts int cRank comm
          -- then broadcast the actual message
@@ -204,7 +204,7 @@ barrier :: Comm -> IO ()
 barrier comm = checkError $ Internal.barrier comm
 
 wait :: Request -> IO Status
-wait request = 
+wait request =
    alloca $ \statusPtr -> do
       checkError $ Internal.wait (castPtr request) (castPtr statusPtr)
-      peek statusPtr 
+      peek statusPtr
