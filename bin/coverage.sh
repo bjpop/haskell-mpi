167bb2f3806388bdd1f2f635c87770572fadca94
Dmitry Astapov <dastapov@gmail.com>
2010-09-16 17:27:11 +0300

    Remove tix files before run

--- bin/coverage.sh
+++ bin/coverage.sh
@@ -4,6 +4,7 @@
 # testsuite. Should be run from the same dir where bindings-mpi.cabal
 # is located and after "cabal install" or "cabal build" has been run
 
+rm -f *.tix
 mpirun -np 2 bindings-mpi-testsuite 2>receiver.log | tee sender.log
 hpc combine --output=bindings-mpi-testsuite.tix rank0.tix rank1.tix
 hpc markup --destdir=./html bindings-mpi-testsuite.tix
