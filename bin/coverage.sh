dc8ba195837171e30f9c2efe7b46b5dbe35299b9
Dmitry Astapov <dastapov@gmail.com>
2010-09-16 18:32:43 +0300

    Increased number of processes used to run testsuite to 5 (to test collective ops properly)

--- bin/coverage.sh
+++ bin/coverage.sh
@@ -5,7 +5,7 @@
 # is located and after "cabal install" or "cabal build" has been run
 
 rm -f *.tix
-mpirun -np 2 bindings-mpi-testsuite 2>receiver.log | tee sender.log
+mpirun -np 5 bindings-mpi-testsuite 2>receivers.log | tee sender.log
 hpc combine --output=bindings-mpi-testsuite.tix rank0.tix rank1.tix
 hpc markup --destdir=./html bindings-mpi-testsuite.tix
 hpc report bindings-mpi-testsuite.tix
