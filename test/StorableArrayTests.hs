7cc54e5f2163d275e2878dbcae9d0ef9e44f65a8
Dmitry Astapov <dastapov@gmail.com>
2010-09-16 18:32:12 +0300

    Scatterv test for testsuit

--- test/StorableArrayTests.hs
+++ test/StorableArrayTests.hs
@@ -1,16 +1,20 @@
+{-# LANGUAGE ScopedTypeVariables #-}
 module StorableArrayTests (storableArrayTests) where
 
 import TestHelpers
 import Control.Parallel.MPI.StorableArray
 import Data.Array.Storable (StorableArray, newListArray, getElems, getBounds)
 
+import Foreign.Storable
+
 
 storableArrayTests :: Rank -> [(String,TestRunnerTest)]
 storableArrayTests rank =
-  [ mpiTestCase rank "Sending (sync)/receiving (sync) simple array" arraySyncRecv
-  , mpiTestCase rank "Broadcast array" arrayBroadcast
+  [ mpiTestCase rank "Sending (sync)/receiving (sync) simple array" syncSendRecvTest
+  , mpiTestCase rank "Broadcast array" broadcastTest
+  , mpiTestCase rank "Scatterv array" scattervTest
   ]
-arraySyncRecv, arrayBroadcast :: Rank -> IO ()
+syncSendRecvTest, broadcastTest, scattervTest :: Rank -> IO ()
 
 -- StorableArray tests
 type ArrMsg = StorableArray Int Int
@@ -22,7 +26,7 @@ range@(low,hi) = (1,10)
 arrMsg :: IO ArrMsg
 arrMsg = newListArray range [low..hi]
 
-arraySyncRecv rank
+syncSendRecvTest rank
   | rank == sender   = do msg <- arrMsg
                           send msg receiver tag2 commWorld
   | rank == receiver = do (status, newMsg) <- recv range sender tag2 commWorld
@@ -31,7 +35,7 @@ arraySyncRecv rank
                           elems == [low..hi::Int] @? "Got wrong array: " ++ show elems
   | otherwise        = return ()
 
-arrayBroadcast _ = do
+broadcastTest _ = do
   msg <- arrMsg
   bs <- getBounds msg
   newMsg <- bcast (msg :: ArrMsg) bs sender commWorld
@@ -39,4 +43,24 @@ arrayBroadcast _ = do
   newElems <- getElems newMsg
   elems == newElems @? "StorableArray bcast yielded garbled result: " ++ show newElems
 
--- End of StorableArray tests
+scattervTest _ = do
+  numProcs <- commSize commWorld
+  let bigRange@(low, hi) = (1, sum [1..numProcs])
+  (msg :: ArrMsg) <- newListArray bigRange [low..hi]
+   
+  myRank <- commRank commWorld
+  let myRankNo = fromRank myRank
+      msgRange = (1, numProcs)
+      counts = [1..numProcs]
+      displs = (0:(Prelude.init $ scanl1 (+) $ [1..numProcs]))
+  (packCounts :: ArrMsg) <- newListArray msgRange $ map (sizeOf (undefined::Int) *) counts
+  (packDispls :: ArrMsg) <- newListArray msgRange $ map (sizeOf (undefined::Int) *) displs
+  
+  let recvRange = (0, myRankNo)
+  segment <- scatterv msg packCounts packDispls recvRange zeroRank commWorld
+  recvMsg <- getElems segment
+  
+  let myCount = counts!!myRankNo
+      myDispl = displs!!myRankNo
+      expected = take myCount $ drop myDispl [low..hi]
+  recvMsg == expected @? "rank = " ++ show myRank ++ " got segment = " ++ show recvMsg ++ " instead of " ++ show expected
