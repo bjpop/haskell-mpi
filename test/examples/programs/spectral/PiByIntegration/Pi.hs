29c2840313c4619e7adf1a8d05a7b949567ae1dd
Bernie Pope <florbitous@gmail.com>
2010-09-16 23:33:29 +1000

    Removed redundant import, made use of mpiWorld, cleaned up code

--- test/examples/programs/spectral/PiByIntegration/Pi.hs
+++ test/examples/programs/spectral/PiByIntegration/Pi.hs
@@ -4,7 +4,6 @@ import Control.Parallel.MPI.Serializable
 import Control.Parallel.MPI.Common
 import Data.Char (isDigit)
 import System.IO (hFlush, stdout)
-import Data.Array.Storable
 import Control.Applicative ((<$>))
 import Control.Monad (when, forM)
 
@@ -15,37 +14,35 @@ tag :: Tag
 tag = toTag ()
 
 main :: IO ()
-main = mpi $ do
-   rank <- commRank commWorld
-   numProcs <- commSize commWorld
+main = mpiWorld $ \procs rank -> do
    n <- if (rank == root)
            then do
               input <- getNumber "Enter number of intervals: "
               bcast input root commWorld
            else
               bcast undefined root commWorld
-   let part = integrate (fromRank rank + 1) numProcs n (1 / fromIntegral n)
+   let part = integrate (fromRank rank + 1) procs n (1 / fromIntegral n)
    send part root tag commWorld
    when (rank == root) $ do
-      pi <- sum <$> gatherAll numProcs
+      pi <- sum <$> gatherAll procs
       print pi
 
 gatherAll :: Int -> IO [Double]
-gatherAll numProcs =
-   forM [0..numProcs-1] $ \rank ->
+gatherAll procs =
+   forM [0..procs-1] $ \rank ->
       snd <$> recv (toRank rank) tag commWorld
 
 integrate :: Int -> Int -> Int -> Double -> Double
-integrate rank numProcs n h =
+integrate rank procs n h =
    -- XXX superfluous type annotation needed to work around
    -- suspected GHC bug, see ticket #4321
    -- http://hackage.haskell.org/trac/ghc/ticket/4321
    -- (nothng to do with MPI)
-   h * (sum (map rectangle steps) :: Double)
+   h * (sum (map area steps) :: Double)
    where
-   steps = [rank, rank + numProcs .. n]
-   rectangle :: Int -> Double
-   rectangle i
+   steps = [rank, rank + procs .. n]
+   area :: Int -> Double
+   area i
       = 4 / (1 + x*x)
       where
       x = h * (fromIntegral i - 0.5)
