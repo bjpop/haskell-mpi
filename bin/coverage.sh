605cade62fc9a3e9540012d3318b5794bc70397a
Dmitry Astapov <dastapov@gmail.com>
2010-09-28 10:25:42 +0300

    Combine coverage files from all 5 ranks

--- bin/coverage.sh
+++ bin/coverage.sh
@@ -6,6 +6,9 @@
 
 rm -f *.tix
 mpirun -np 5 bindings-mpi-testsuite 2>receivers.log | tee sender.log
-hpc combine --output=bindings-mpi-testsuite.tix rank0.tix rank1.tix
+hpc combine --output=rank01.tix rank0.tix rank1.tix
+hpc combine --output=rank23.tix rank2.tix rank3.tix
+hpc combine --output=rank0123.tix rank01.tix rank23.tix
+hpc combine --output=bindings-mpi-testsuite.tix rank0123.tix rank4.tix
 hpc markup --destdir=./html bindings-mpi-testsuite.tix
 hpc report bindings-mpi-testsuite.tix
