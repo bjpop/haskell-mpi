bcecbbe70e6d7cf156e4f39a361b1e7e8679d090
Dmitry Astapov <dastapov@gmail.com>
2010-10-03 00:03:44 +0300

    intoBool, intoEnum, intoInt helpers (to simplify code in Common.hs)

--- src/Control/Parallel/MPI/Utils.hs
+++ src/Control/Parallel/MPI/Utils.hs
@@ -1,6 +1,6 @@
-module Control.Parallel.MPI.Utils (checkError) where
+module Control.Parallel.MPI.Utils (checkError, intoBool, intoInt, intoEnum) where
 
-import Foreign.C.Types (CInt)
+import C2HS
 import Control.Monad (unless)
 import Control.Applicative ((<$>))
 import Control.Exception.Extensible (throwIO)
@@ -11,3 +11,24 @@ checkError :: IO CInt -> IO ()
 checkError comp = do
    errorClass <- enumFromCInt <$> comp
    unless (errorClass == Success) (throwIO errorClass)
+
+intoBool :: (Ptr CInt -> IO ()) -> IO Bool
+intoBool f =
+  alloca $ \ptr -> do
+    f ptr
+    res <- peek ptr
+    return $ res /= 0
+
+intoInt :: (Ptr CInt -> IO ()) -> IO Int
+intoInt f =
+  alloca $ \ptr -> do
+    f ptr
+    res <- peek ptr
+    return $ cIntConv res
+    
+intoEnum :: Enum a => (Ptr CInt -> IO ()) -> IO a
+intoEnum f =
+  alloca $ \ptr -> do
+    f ptr
+    res <- peek ptr
+    return $ enumFromCInt res
