fc3169dccaaf48bcddcd65f3c3d5ef3bbc87dc3d
Bernie Pope <florbitous@gmail.com>
2010-10-07 19:34:35 +1100

    Modified Greetings test to use gather, and take advantage of Num instance of rank

--- test/examples/programs/imaginary/Greetings.hs
+++ test/examples/programs/imaginary/Greetings.hs
@@ -3,17 +3,14 @@
 
 module Main where
 
-import Control.Monad
-import Control.Applicative
 import Control.Parallel.MPI.Serializable
 import Control.Parallel.MPI.Common
 
 main :: IO ()
-main = mpiWorld $ \size rank ->
-   if rank /= zeroRank
-      then send commWorld zeroRank unitTag $ msg rank
-      else forM_ [1..size-1] $ \sender ->
-              putStrLn =<< fst <$> recv commWorld (toRank sender) unitTag
+main = mpiWorld $ \_size rank ->
+   if rank == 0
+      then mapM_ putStrLn =<< (recvGather commWorld 0 $ msg rank)
+      else sendGather commWorld 0 $ msg rank
 
 msg :: Rank -> String
 msg r = "Greetings from process " ++ show r ++ "!"
