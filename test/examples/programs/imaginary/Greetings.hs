255706fb8595c66833177111f2120fce174c2075
Bernie Pope <florbitous@gmail.com>
2010-10-07 19:38:22 +1100

    Let bound root = 0 to make code clearer

--- test/examples/programs/imaginary/Greetings.hs
+++ test/examples/programs/imaginary/Greetings.hs
@@ -7,10 +7,11 @@ import Control.Parallel.MPI.Serializable
 import Control.Parallel.MPI.Common
 
 main :: IO ()
-main = mpiWorld $ \_size rank ->
-   if rank == 0
-      then mapM_ putStrLn =<< (recvGather commWorld 0 $ msg rank)
-      else sendGather commWorld 0 $ msg rank
+main = mpiWorld $ \_size rank -> do
+   let root = 0
+   if rank == root
+      then mapM_ putStrLn =<< (recvGather commWorld root $ msg rank)
+      else sendGather commWorld root $ msg rank
 
 msg :: Rank -> String
 msg r = "Greetings from process " ++ show r ++ "!"
