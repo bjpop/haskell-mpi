d152f714764f69b9579f928994244442b41c2f0d
Dmitry Astapov <dastapov@gmail.com>
2010-10-14 14:31:33 +0300

    Fix minBound/maxBound test to work with arbitrary number of testing processes

--- test/PrimTypeTests.hs
+++ test/PrimTypeTests.hs
@@ -10,12 +10,16 @@ primTypeTests :: Rank -> [(String,TestRunnerTest)]
 primTypeTests rank =
   [ mpiTestCase rank "intMaxBound" (sendRecvSingleValTest (maxBound :: Int))
   , mpiTestCase rank "intMinBound" (sendRecvSingleValTest (minBound :: Int))
+  , mpiTestCase rank "checking sizes of Haskell types vs MPI representations" reprSizeTest
   ]
 
 sendRecvSingleValTest :: forall a . (RecvInto (Ptr a), Repr a, SendFrom a, Storable a, Eq a, Show a) => a -> Rank -> IO ()
-sendRecvSingleValTest val rank =
-   if rank == 0
-      then send commWorld 1 unitTag (val :: a)
-      else do
-         (result :: a, _status) <- intoNewVal $ recv commWorld 0 unitTag
-         result == val @? "result: " ++ show result ++ " not equal to sent val: " ++ show (val :: a)
+sendRecvSingleValTest val rank 
+  | rank == 0 = send commWorld 1 unitTag (val :: a)
+  | rank == 1 = do
+    (result :: a, _status) <- intoNewVal $ recv commWorld 0 unitTag
+    result == val @? "result: " ++ show result ++ " not equal to sent val: " ++ show (val :: a)
+  | otherwise = return ()
+
+reprSizeTest _ = do
+  sizeOf (undefined :: Int) == (typeSize int) @? "Size of Int differs from size of MPI_INT"
