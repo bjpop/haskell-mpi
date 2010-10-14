3f91ae3885f03064b4a2131621ff9c60a245e6ab
Bernie <florbitous@gmail.com>
2010-10-14 18:24:02 +1100

    generalised the test case to allow send/recv of arbitrary value

--- test/PrimTypeTests.hs
+++ test/PrimTypeTests.hs
@@ -1,19 +1,21 @@
-{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE ScopedTypeVariables, FlexibleContexts #-}
 
 module PrimTypeTests (primTypeTests) where
 
 import TestHelpers
 import Control.Parallel.MPI.Storable
+import C2HS
 
 primTypeTests :: Rank -> [(String,TestRunnerTest)]
 primTypeTests rank =
-  [ mpiTestCase rank "intMaxBound" intMaxBoundTest
+  [ mpiTestCase rank "intMaxBound" (sendRecvSingleValTest (maxBound :: Int))
+  , mpiTestCase rank "intMinBound" (sendRecvSingleValTest (minBound :: Int))
   ]
 
-intMaxBoundTest :: Rank -> IO ()
-intMaxBoundTest rank =
+sendRecvSingleValTest :: forall a . (RecvInto (Ptr a), Repr a, SendFrom a, Storable a, Eq a, Show a) => a -> Rank -> IO ()
+sendRecvSingleValTest val rank =
    if rank == 0
-      then send commWorld 1 unitTag (maxBound :: Int)
+      then send commWorld 1 unitTag (val :: a)
       else do
-         (result :: Int, _status) <- intoNewVal $ recv commWorld 0 unitTag
-         result == maxBound @? "result: " ++ show result ++ " not equal to maxBound: " ++ show (maxBound :: Int)
+         (result :: a, _status) <- intoNewVal $ recv commWorld 0 unitTag
+         result == val @? "result: " ++ show result ++ " not equal to sent val: " ++ show (val :: a)
