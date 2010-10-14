f0f5e431f065bd2dbfc79bfdd5ac74c51e58cf4e
Dmitry Astapov <dastapov@gmail.com>
2010-10-14 15:03:21 +0300

    Tests for min and max values of various Int and Word flavors

--- test/PrimTypeTests.hs
+++ test/PrimTypeTests.hs
@@ -8,8 +8,18 @@ import C2HS
 
 primTypeTests :: Rank -> [(String,TestRunnerTest)]
 primTypeTests rank =
-  [ mpiTestCase rank "intMaxBound" (sendRecvSingleValTest (maxBound :: Int))
-  , mpiTestCase rank "intMinBound" (sendRecvSingleValTest (minBound :: Int))
+  [ mpiTestCase rank "Int maxBound" (sendRecvSingleValTest (maxBound :: Int))
+  , mpiTestCase rank "Int minBound" (sendRecvSingleValTest (minBound :: Int))
+  , mpiTestCase rank "Int32 maxBound" (sendRecvSingleValTest (maxBound :: Int32))
+  , mpiTestCase rank "Int32 minBound" (sendRecvSingleValTest (minBound :: Int32))    
+  , mpiTestCase rank "Int64 maxBound" (sendRecvSingleValTest (maxBound :: Int64))
+  , mpiTestCase rank "Int64 minBound" (sendRecvSingleValTest (minBound :: Int64))
+  , mpiTestCase rank "Word maxBound" (sendRecvSingleValTest (maxBound :: Word))
+  , mpiTestCase rank "Word minBound" (sendRecvSingleValTest (minBound :: Word))
+  , mpiTestCase rank "Word32 maxBound" (sendRecvSingleValTest (maxBound :: Word32))
+  , mpiTestCase rank "Word32 minBound" (sendRecvSingleValTest (minBound :: Word32))    
+  , mpiTestCase rank "Word64 maxBound" (sendRecvSingleValTest (maxBound :: Word64))
+  , mpiTestCase rank "Word64 minBound" (sendRecvSingleValTest (minBound :: Int64))
   , mpiTestCase rank "checking sizes of Haskell types vs MPI representations" reprSizeTest
   ]
 
